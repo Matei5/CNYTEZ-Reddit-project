ffd156fe54809aa08b5c8852469e0b50848d2d6d
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-07-05 13:01:19 +0300

    written initial startup menu

--- matei/src/ui/ConsoleView.java
+++ matei/src/ui/ConsoleView.java
@@ -1,4 +1,21 @@
 package ui;
 
+import model.Comment;
+import model.Post;
+import model.Subreddit;
+import model.User;
+
+import java.util.List;
+
 public class ConsoleView {
+    public void ShowGuestMenu(){
+        System.out.println("=".repeat(40));
+        System.out.println("Welcome to Cnytez's Reddit!");
+        System.out.println("1. Register");
+        System.out.println("2. Login");
+        System.out.println("0. Exit");
+        System.out.println("Choose an option (0-2): ");
+    }
+
+
 }
