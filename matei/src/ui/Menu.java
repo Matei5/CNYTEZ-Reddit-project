66d70a7bd5da90bfc5f53582b1454720633b9d93
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-07-05 13:02:40 +0300

    written constructor and future methods to implement for the menu class

--- matei/src/ui/Menu.java
+++ matei/src/ui/Menu.java
@@ -1,4 +1,69 @@
 package ui;
 
+import service.AuthService;
+import service.SubredditService;
+import service.PostService;
+import service.CommentService;
+
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner scanner;
+
+    private AuthService authService;
+    private SubredditService subredditService;
+    private PostService postService;
+    private CommentService commentService;
+
+    public Menu(AuthService authSerrvice, SubredditService subredditService,
+                PostService postService, CommentService commentService){
+        this.scanner = new Scanner(System.in);
+        this.authService = authService;
+        this.subredditService = subredditService;
+        this.postService = postService;
+        this.commentService = commentService;
+    }
+
+    public void start() {
+    }
+
+    private void showGuestMenu() {
+    }
+
+    private void showUserMenu() {
+    }
+
+    private void handleRegister() {
+    }
+
+    private void handleLogin() {
+    }
+
+    private void handleLogout() {
+    }
+
+    private void handleCreateSubreddit() {
+    }
+
+    private void handleListSubreddits() {
+    }
+
+    private void handleCreatePost() {
+    }
+
+    private void handleViewPosts() {
+    }
+
+    private void handleViewPostDetails() {
+    }
+
+    private void handleCreateComment() {
+    }
+
+    private void handleDeleteComment() {
+    }
+
+    private void handleDeletePost() {
+    }
+
 }
