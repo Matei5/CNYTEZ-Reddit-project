821bfe89b4ae458c7540d963c3652c3b68ffa797
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-07-06 08:36:15 +0300

    Partially implemented Comment Service class - instance and create

--- matei/src/service/CommentService.java
+++ matei/src/service/CommentService.java
@@ -1,4 +1,38 @@
 package service;
 
+import model.Comment;
+import repository.CommentRepository;
+
 public class CommentService {
+    private static CommentService instance;
+
+    private static int currentId;
+    private CommentRepository commentRepository;
+
+    public CommentService() {
+        this.repo = CommentRepository.getInstance();
+        currentId = 1;
+    }
+
+    public static CommentService getInstance() {
+        if (instance == null) {
+            instance = new CommentService();
+        }
+        return instance;
+    }
+
+    public boolean createComment(int parentPostID, int parentCommentID, int ownerId,
+                              String title, String content,String image) {
+        User loggedUser = AuthService.getLoggedInUser();
+        if (loggedUser == null) {
+            return false;
+        }
+
+        int ownerId = loggedUser.getId();
+
+        Comment comment = new Comment(currentId++, parentPostID, parentCommentID, ownerId, title, content, image);
+
+        commentRepository.addComment(comment);
+        return true;
+    }
 }
