111695e1b927da55eb5100d9a3223f2396774e49
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-07-06 08:11:31 +0300

    Implemented Comment repository class

--- matei/src/repository/CommentRepository.java
+++ matei/src/repository/CommentRepository.java
@@ -1,4 +1,46 @@
 package repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Comment;
+
 public class CommentRepository {
+    private static CommentRepository instance;
+    private List<Comment> comments;
+
+    private CommentRepository() {
+        comments = new ArrayList<Comment>();
+    }
+
+    public static CommentRepository getInstance() {
+        if (instance == null) {
+            instance = new CommentRepository();
+        }
+        return instance;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeById(int id) {
+        comments.removeIf(comment -> comment.getID() == id);
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public Comment findById(int id) {
+        for (Comment comment : comments) {
+            if (comment.getID() == id){
+                return comment;
+            }
+        }
+
+        return null;
+    }
+
+
 }
