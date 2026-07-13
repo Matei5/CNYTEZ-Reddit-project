74e8179dc0453e4cf4d0fe72760ab9435ed49f5e
Matei5 <147913718+Matei5@users.noreply.github.com>
2026-07-13 13:50:11 +0300

    Implemented comment, post and subreddit json file saving

--- App/src/main/java/repository/JsonFileCommentRepository.java
+++ App/src/main/java/repository/JsonFileCommentRepository.java
@@ -1,4 +1,107 @@
 package repository;
 
-public class JsonFileCommentRepository {
+import adapter.LocalDateTimeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import model.Post;
+import log.LogManager;
+import model.Comment;
+import model.Subreddit;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
+
+public class JsonFileCommentRepository implements CommentRepository {
+    private static JsonFileCommentRepository instance;
+    private final List<Comment> comments;
+
+    private static final String FILE_PATH = "data/comments.json";
+    private final Gson gson;
+
+    private JsonFileCommentRepository() {
+        gson = new GsonBuilder().setPrettyPrinting()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+        comments = new ArrayList<Comment>();
+    }
+
+    public static JsonFileCommentRepository getInstance() {
+        if (instance == null) {
+            instance = new JsonFileCommentRepository();
+        }
+        return instance;
+    }
+
+    private List<Comment> loadFromFile(){
+        File file = new File(FILE_PATH);
+        if(!file.exists()){ return new ArrayList<>(); }
+
+        try (Reader reader = new BufferedReader(new FileReader(file))){
+            Type listType = new TypeToken<ArrayList<Subreddit>>(){}.getType();
+            List<Comment> comments = gson.fromJson(reader,listType);
+            if(comments.isEmpty()){ return new ArrayList<>(); }
+            else { return comments; }
+        } catch (IOException e){
+            LogManager.getInstance().log("Failed to load subreddits: " + e.getMessage());
+            return new ArrayList<>();
+
+        }
+    }
+
+    private void saveToFile(){
+        File file = new File(FILE_PATH);
+        file.getParentFile().mkdirs();
+
+        try (Writer writer = new BufferedWriter(new FileWriter(file))){
+            gson.toJson(comments, writer);
+        } catch (IOException e){
+            LogManager.getInstance().log("Failed to save subreddits: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void addComment(Comment comment) {
+        comments.add(comment);
+        saveToFile();
+    }
+
+    @Override
+    public void removeById(int id) {
+        boolean removed = comments.removeIf(comment -> comment.getID() == id);
+        if (removed) saveToFile();;
+    }
+
+    @Override
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    @Override
+    public Comment findById(int id) {
+        for (Comment comment : comments) {
+            if (comment.getID() == id){
+                return comment;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Comment> getCommentsByUser(int userId) {
+        List<Comment> userComments = new ArrayList<>();
+
+        for (Comment comment : comments) {
+            if (comment.getOwnerID() == userId) {
+                userComments.add(comment);
+            }
+        }
+
+        return userComments;
+    }
 }
